67757cecb8e17913b3378c0e9105ae1c3c980855
Thijs de Vries <Thijs.s.devries@gmail.com>
2025-12-28 22:08:37 +0100

    fix: add dev domain to convex config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -6,5 +6,9 @@ export default {
       domain: "https://clerk.auto-m8.app",
       applicationID: "convex",
     },
+    {
+      domain: "https://singular-malamute-43.clerk.accounts.dev",
+      applicationID: "convex",
+    },
   ],
 };
